f62e89bd8eea3a303009b341a00c332b7bb90712
Tau Gärtli <git@tau.garden>
2024-07-16 16:12:56 +0200

    Deploy demo to GitHub Pages

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,7 +1,10 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use eframe::{Frame, Theme};
 use egui::{vec2, CentralPanel, Context, ViewportBuilder, ViewportCommand};
 use egui_theme_switch::{ThemePreference, ThemeSwitch};
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
     let system_theme = system_theme();
     let default_theme = system_theme.unwrap_or(Theme::Light);
@@ -20,6 +23,42 @@ fn main() -> eframe::Result {
         options,
         Box::new(move |_cc| Ok(Box::new(ThemeSwitchDemoApp::new(system_theme)))),
     )
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let start_result = eframe::WebRunner::new()
+            .start(
+                "egui-app",
+                web_options,
+                Box::new(|cc| Ok(Box::new(ThemeSwitchDemoApp::new(None)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        let loading_text = web_sys::window()
+            .and_then(|w| w.document())
+            .and_then(|d| d.get_element_by_id("loading_text"));
+        if let Some(loading_text) = loading_text {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
 
 struct ThemeSwitchDemoApp {
@@ -42,6 +81,8 @@ impl eframe::App for ThemeSwitchDemoApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut Frame) {
         CentralPanel::default().show(ctx, |ui| {
             ui.vertical_centered(|ui| {
+                ui.heading("Theme Switch Demo");
+                ui.add_space(2.0);
                 if ui.add(ThemeSwitch::new(&mut self.preference)).changed() {
                     self.apply_theme_preference(ctx, frame);
                 }
